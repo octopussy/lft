51af132b28a52c2059e8d80164efbcc702891e58
octopussy <uberpilot@gmail.com>
2020-04-05 01:02:50 +0300

    start testing Elwynn

--- Core.lua
+++ Core.lua
@@ -15,15 +15,26 @@ Rows = {}
 local trackerFrame
 local actionFrame
 
-local function OnUpdate(self, elapsed)
-    local m,x,y = LFT:GetCurrentPlayerPosition()
-    local mapName = LFT.HBD:GetLocalizedMap(m)
-    local str = string.format("%s - %d (%f, %f)", mapName, m, x, y)
-    --print(str)
-    --trackerFrame.DebugText:SetText(str)
+local wayframe = CreateFrame("Button", "CrazyArrow", UIParent)
+wayframe:SetHeight(42)
+wayframe:SetWidth(56)
+wayframe:EnableMouse(true)
+wayframe:SetMovable(true)
+wayframe:SetClampedToScreen(true)
+wayframe:Show()
 
-    --LFTDB.Data = str
-end
+--wayframe:SetScript("OnDragStart", OnDragStart)
+--wayframe:SetScript("OnDragStop", OnDragStop)
+wayframe:RegisterForDrag("LeftButton")
+wayframe:RegisterEvent("ZONE_CHANGED_NEW_AREA")
+wayframe:RegisterEvent("ZONE_CHANGED")
+wayframe:RegisterEvent("PLAYER_ENTERING_WORLD")
+wayframe:SetScript("OnEvent", OnEvent)
+
+wayframe.arrow = wayframe:CreateTexture(nil, "OVERLAY")
+wayframe.arrow:SetTexture("Interface\\Addons\\TomTom\\Images\\Arrow")
+wayframe.arrow:SetAllPoints()
+wayframe.arrow:Show()
 
 local function GetNearestItem()
     local nearestPoint = nil
@@ -79,9 +90,81 @@ local function UpdateBars()
         return a.time < b.time
     end)
 
+    local rowIndex = 1
     for c, e in pairs(visitedItems) do
-        --print(c)
-        print(e.id .. ": " .. e.time)
+
+        local x, y = LFT:DecodeLoc(e.id)
+        local coords = RoundCoords(x, y, 2)
+
+       -- print(e.id .. ": " .. e.time)
+        if rowIndex > #Rows then
+            local rowHeight = 16
+            local row = CreateFrame("Button", "LFT_Bar"..rowIndex, trackerFrame)
+            row:SetPoint("TOPLEFT", trackerFrame, "TOPLEFT", 2, -32 - (rowHeight) * (rowIndex - 1))
+	        row:SetHeight(rowHeight)
+            row:SetWidth(trackerFrame:GetWidth() - 4)
+            row.StatusBar = CreateFrame("StatusBar", nil, row)
+            row.StatusBar:SetAllPoints(row)
+            
+            local BarTexture = SM:Fetch("statusbar", "BantoBar")
+            row.StatusBar:SetStatusBarTexture(BarTexture)
+            row.StatusBar:GetStatusBarTexture():SetHorizTile(false)
+            row.StatusBar:GetStatusBarTexture():SetVertTile(false)
+            row.StatusBar:SetStatusBarColor(5, .5, .5, 1)
+            row.StatusBar:SetMinMaxValues(0, 100)
+            row.StatusBar:SetValue(100)
+            row.StatusBar:Show()
+
+            row.LeftText = row.StatusBar:CreateFontString(nil, "OVERLAY", "GameFontHighlightSmall")
+            row.LeftText:SetPoint("LEFT", row.StatusBar, "LEFT", 2, 0)
+            row.LeftText:SetJustifyH("LEFT")
+            row.LeftText:SetText("Test")
+            row.LeftText:SetTextColor(1, 1, 1, 1)
+            row.LeftText:SetWordWrap(false)
+
+            row.RightText = row.StatusBar:CreateFontString(nil, "OVERLAY", "GameFontHighlightSmall")
+            row.RightText:SetPoint("RIGHT", row.StatusBar, "RIGHT", -2, 0)
+            row.RightText:SetJustifyH("RIGHT")
+            row.RightText:SetText("0")
+            row.RightText:SetTextColor(1, 1, 1, 1)
+
+            row:EnableMouse(true)
+            row:RegisterForClicks("LeftButtonDown")
+            row:SetScript("OnClick", function(self, button)
+                local editbox=ChatEdit_ChooseBoxForSend(DEFAULT_CHAT_FRAME);--  Get an editbox
+                ChatEdit_ActivateChat(editbox);--   Show the editbox
+                editbox:SetText("/way " .. coords);-- Command goes here
+                ChatEdit_OnEnterPressed(editbox);-- Process command and hide (runs ChatEdit_SendText() and ChatEdit_DeactivateChat() respectively)
+            end)
+            
+            table.insert(Rows, row)
+        end
+
+        local row = Rows[rowIndex]
+
+        if not row:IsShown() then
+            row:Show()
+        end
+
+
+        --print(#Rows)
+        --print(row.StatusBar)
+        --print(row.LeftText)
+
+        --print(e.id)
+        --print(e.time)
+
+        local now = GetServerTime()
+        local elapsed = now - e.time
+
+        row.LeftText:SetText(coords)
+        row.RightText:SetFormattedText("%d s", elapsed)
+
+        rowIndex = rowIndex + 1
+    end
+
+    for i=rowIndex, #Rows do
+        Rows[i]:Hide()
     end
 end
 
@@ -94,20 +177,28 @@ local function OnGatherClicked()
     local now = GetServerTime()
     LFTDB[item] = now
     print("Gathered: " .. item .. " at " .. now)
+end
+
+local function OnUpdate(self, elapsed)
+    local m,x,y = LFT:GetCurrentPlayerPosition()
+    local mapName = LFT.HBD:GetLocalizedMap(m)
+    local str = string.format("%s - %d (%f, %f)", mapName, m, x, y)
+    --print(str)
+    --trackerFrame.DebugText:SetText(str)
+
+    --LFTDB.Data = str
 
     UpdateBars()
 end
 
-
-local function initData()
+local function initData(resetTimers)
     for i, v in pairs(Herbs) do
-        if v == Peacebloom and not LFTDB[i] then
+        if v == Peacebloom and (not LFTDB[i] or resetTimers) then
             LFTDB[i] = 0
            -- print(i .. ": " .. LFTDB[i])
         end
-    end
 
-    UpdateBars()
+    end
 end
 
 local function createActionFrame()
@@ -159,6 +250,16 @@ local function createActionFrame()
     btnGather:SetScript("OnClick", OnGatherClicked)
     btnGather:Show()
     actionFrame.btnGather = btnGather
+
+    local btnReset = CreateFrame("Button", "Btn_Reset", actionFrame, "UIPanelButtonTemplate")
+    btnReset:SetPoint("TOPLEFT", actionFrame, "TOPLEFT", 0, -40)
+    btnReset:SetWidth(100)
+    btnReset:SetHeight(20)
+    btnReset:SetScript("OnClick", function()
+        initData(true)
+    end)
+    btnReset:Show()
+    actionFrame.btnReset = btnReset
 end
 
 local function createTrackerFrame()
@@ -226,44 +327,10 @@ function LFT:OnInitialize()
         --fs:SetText("123123123")
     end
 
-    initData()
+    initData(false)
 
-    --self.db = LibStub("AceDB-3.0"):New("LFTDB", self.defaults)
-
-    local num = 1
-
-    local row = CreateFrame("Button", "LFT_Tracker_Bar"..num, trackerFrame)
-    local rowHeight = 16
-    local rowSpacing = 2
-
-	row:SetPoint("TOPLEFT", trackerFrame, "TOPLEFT", 2, -16 - (rowHeight + rowSpacing) * (num - 1))
-	row:SetHeight(rowHeight)
-    row:SetWidth(trackerFrame:GetWidth() - 4)
-
-    row.StatusBar = CreateFrame("StatusBar", nil, row)
-    row.StatusBar:SetAllPoints(row)
-
-    local BarTexture = SM:Fetch("statusbar", "BantoBar")
-
-    row.StatusBar:SetStatusBarTexture(BarTexture)
-	row.StatusBar:GetStatusBarTexture():SetHorizTile(false)
-	row.StatusBar:GetStatusBarTexture():SetVertTile(false)
-	row.StatusBar:SetStatusBarColor(5, .5, .5, 1)
-	row.StatusBar:SetMinMaxValues(0, 100)
-	row.StatusBar:SetValue(50)
-	row.StatusBar:Show()
-
-
-    row.LeftText = row.StatusBar:CreateFontString(nil, "OVERLAY", "GameFontHighlightSmall")
-	row.LeftText:SetPoint("LEFT", row.StatusBar, "LEFT", 2, 0)
-	row.LeftText:SetJustifyH("LEFT")
-	row.LeftText:SetText("Test")
-	row.LeftText:SetTextColor(1, 1, 1, 1)
-	row.LeftText:SetWordWrap(false)
 	--me:SetFontSize(row.LeftText, math_max(rowHeight * 0.75, rowHeight - 3))
 	--Recount:AddFontString(row.LeftText)
-
-    row:Show()
 
     print("LFT initialized")
 end
